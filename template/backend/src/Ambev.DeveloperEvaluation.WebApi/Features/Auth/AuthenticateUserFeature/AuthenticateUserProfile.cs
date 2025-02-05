75737444063bff74da0e03b693fbd5ec45b66b49
Vinicius Rocha <rochavini@outlook.com>
2025-02-05 15:20:24 -0300

    fix -> Adds missing mappers for user, and authentication

--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Auth/AuthenticateUserFeature/AuthenticateUserProfile.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Auth/AuthenticateUserFeature/AuthenticateUserProfile.cs
@@ -1,3 +1,4 @@
+using Ambev.DeveloperEvaluation.Application.Auth.AuthenticateUser;
 using AutoMapper;
 using Ambev.DeveloperEvaluation.Domain.Entities;
 
@@ -16,5 +17,8 @@ public sealed class AuthenticateUserProfile : Profile
         CreateMap<User, AuthenticateUserResponse>()
             .ForMember(dest => dest.Token, opt => opt.Ignore())
             .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role.ToString()));
+        
+        CreateMap<AuthenticateUserRequest, AuthenticateUserCommand>();
+        CreateMap<AuthenticateUserResult, AuthenticateUserResponse>();
     }
 }
