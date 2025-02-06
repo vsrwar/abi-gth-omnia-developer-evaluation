f9476219e6ab0844a90e71f6d328298af5841133
Vinicius Rocha <rochavini@outlook.com>
2025-02-06 18:13:54 -0300

    Adds sale entity and its dependencies, also adds discount business rule

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Services/Discount/Discount20Percent.cs
@@ -0,0 +1,28 @@
+﻿using Ambev.DeveloperEvaluation.Domain.ValueObjects;
+
+namespace Ambev.DeveloperEvaluation.Domain.Services.Discount;
+
+/// <summary>
+/// Handles if a 20% discount will be applied or not
+/// This class belongs to a chain handled in <see cref="DiscountHandler"/>
+/// </summary>
+internal class Discount20Percent : DiscountCalculator
+{
+    private const int DiscountPercentage = 20;
+
+    public Discount20Percent(DiscountCalculator? next) : base(next)
+    {
+    }
+
+    public override SaleDiscount? Calculate(IEnumerable<SaleProduct> saleProducts)
+    {
+        return CanHandle(saleProducts)
+            ? Handle(saleProducts, DiscountPercentage)
+            : Next?.Calculate(saleProducts);
+    }
+
+    protected override bool CanHandle(IEnumerable<SaleProduct> saleProducts)
+    {
+        return saleProducts.Any(sp => sp.Quantity is >= 10 and < 20);
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Services/Discount/DiscountCalculator.cs
@@ -0,0 +1,46 @@
+﻿using Ambev.DeveloperEvaluation.Domain.ValueObjects;
+
+namespace Ambev.DeveloperEvaluation.Domain.Services.Discount;
+
+/// <summary>
+/// This class has discount formula on Handle method, and is used 
+/// to implement chain of responsibility design pattern.
+/// <para>Handled by <see cref="DiscountHandler"/></para>
+/// </summary>
+/// <remarks>See also <see href="https://refactoring.guru/design-patterns/chain-of-responsibility"/></remarks>
+public abstract class DiscountCalculator
+{
+    protected readonly DiscountCalculator? Next;
+    
+    protected DiscountCalculator(DiscountCalculator? next)
+    {
+        Next = next;
+    }
+
+    /// <summary>
+    /// Starts the chain by checking if this instance can handle the discount, or if Next one should be called.
+    /// </summary>
+    /// <param name="saleProducts">A list of <see cref="SaleProduct"/>.</param>
+    /// <returns>A new instance of <see cref="SaleDiscount"/> containing discount value and percentage.</returns>
+    /// <returns> Or null if none of the child class can handle discount calculation.</returns>
+    public abstract SaleDiscount? Calculate(IEnumerable<SaleProduct> saleProducts);
+
+    /// <summary>
+    /// Calculates the discount based on products * quantity * percentage / 100
+    /// </summary>
+    /// <param name="saleProducts">A list of <see cref="SaleProduct"/>.</param>
+    /// <param name="percentage">Percentage to be apllied.</param>
+    /// <returns>A new instance of <see cref="SaleDiscount"/> containing discount value and percentage.</returns>
+    protected static SaleDiscount Handle(IEnumerable<SaleProduct> saleProducts, int percentage)
+    {
+        var value = saleProducts.Sum(sp => sp.Quantity * sp.Price) * percentage / 100;
+        return new SaleDiscount(percentage, value);
+    }
+    
+    /// <summary>
+    /// Checks if this instance of <see cref="DiscountCalculator"/> is able to calculate the discount.
+    /// </summary>
+    /// <param name="saleProducts">A list of <see cref="SaleProduct"/>.</param>
+    /// <returns>True if is the correct instance to apply discount, false otherwise.</returns>
+    protected abstract bool CanHandle(IEnumerable<SaleProduct> saleProducts);
+}
